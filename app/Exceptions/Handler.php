d83f333ed7791ca61d1d86307036c5a3bae39a25
omehmeti <omehmeti@gmail.com>
2016-02-14 23:37:59 +0200

    Handling not found and unexpected exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+        if( $e instanceof NotFoundHttpException){
+            return response()->json(['message'=>'Bad request.Please verify your request route.','code'=>400],400);
+        }else{
+            return response()->json(['message'=>'Unexpected error. Try again later','code'=>500],500);
+        }
+        
     }
 }
