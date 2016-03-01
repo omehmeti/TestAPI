c63a151c506659a97633fc71a9751670ffd45edd
omehmeti <omehmeti@gmail.com>
2016-03-01 05:25:01 +0200

    credential verification is done with email or username

--- app/Http/Controllers/PasswordVerifier.php
+++ app/Http/Controllers/PasswordVerifier.php
@@ -6,13 +6,14 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use App\User;
 use Auth;
+use Illuminate\Support\Facades\Hash;
 class PasswordVerifier extends Controller
 {
     public function verify($username, $password)
     {
-    	$credentials = 
+    /*	$credentials = 
     	[
     		'email' => $username,
     		'password' => $password,
@@ -23,7 +24,34 @@ class PasswordVerifier extends Controller
     }
 
     return false;
+    */
+    //Checking if exists user by email or username
 
+    $user = User::where('email', $username)->orWhere('username', $username)->first();
+
+    if($user)
+
+    {
+
+        //If exists so check password
+
+        if(Hash::check($password, $user->password))
+
+        {
+
+            //if match, so return the user id.
+
+            return $user->id;
+
+        }
+
+    //If not, so continue to return false
+
+    }
+
+    //If does not exists return false
+
+    return false;
     }
 
 
